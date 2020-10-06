0a8271372a5196f05c8d90741c7e9a43e3f6f748
Dan Kreiger <dan@dankreiger.com>
2020-10-06 21:58:55 +0200

    chore: format js

--- insertionSort/insertionSort.spec.js
+++ insertionSort/insertionSort.spec.js
@@ -1,7 +1,7 @@
-const insertionSort = require("./insertionSort");
+const insertionSort = require('./insertionSort');
 
-describe("insertionSort", () => {
-  test("sorts an unsorted array", () => {
+describe('insertionSort', () => {
+  test('sorts an unsorted array', () => {
     expect(insertionSort([3, 2, 10, 100, 74])).toEqual([2, 3, 10, 74, 100]);
     expect(insertionSort([3])).toEqual([3]);
   });
--- mergeSort/mergeSort.spec.js
+++ mergeSort/mergeSort.spec.js
@@ -1,7 +1,7 @@
-const mergeSort = require("./mergeSort");
+const mergeSort = require('./mergeSort');
 
-describe("mergeSort", () => {
-  test("sorts an unsorted array", () => {
+describe('mergeSort', () => {
+  test('sorts an unsorted array', () => {
     expect(mergeSort([3, 2, 10, 100, 74])).toEqual([2, 3, 10, 74, 100]);
     expect(mergeSort([3])).toEqual([3]);
   });
